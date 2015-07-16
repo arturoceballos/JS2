4d29fe7d78f3f9bc9917d3bc4b27c9bf4c8f288e
arturoceballos <aceballos118@gmail.com>
2015-07-16 11:38:51 -0700

    Outlined all html steps. Outlined all js steps. For both finished and review

--- angularjs/angular-day-2/review/js/app.js
+++ angularjs/angular-day-2/review/js/app.js
@@ -1,9 +1,14 @@
-//step 1: Create your app module
+//Step 1: Create your app module
 
-//step 2: Create 3 js objects. Your Gems must have a name, description,  price, images, and shine
+//Step 2: Create 3 js objects. Your Gems must have a name, description,  price, images, and shine
 
-//step 3: Create your StoreController
+//Step 3: Create your StoreController
 
-//step 4: Create your TabController
+//Step 4: Create your TabController
+//Step 5: Set your tab to a specific value
+//Step 6: Create function (method) that sets tab to new value when clicked
+//Step 7: Create function (method) that sets the selected tab to an active class to see which tab is selected
 
-//step 5: Create your GalleryController
+//Do the same thing with your images
+//Step 8: Create your GalleryController
+
