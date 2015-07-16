1530aa8f0cef3143bb4df57aea9eb84c258c61ff
arturoceballos <aceballos118@gmail.com>
2015-07-16 09:59:52 -0700

    Added images to app

--- angularjs/myApp/js/app.js
+++ angularjs/myApp/js/app.js
@@ -3,10 +3,9 @@
 
     app.controller('GalleryController', function(){
         this.current = 0;
-
         this.setCurrent = function(newGallery){
             this.current = newGallery || 0;
-        }
+        };
     });
 
     app.controller('StoreController', function(){
@@ -35,9 +34,9 @@
             color: '#CCC',
             faces: 14,
             images: [
-                "images/gem-02.gif",
-                "images/gem-05.gif",
-                "images/gem-09.gif"
+                "img/gem-02.gif",
+                "img/gem-05.gif",
+                "img/gem-09.gif"
             ],
             reviews: [{
                 stars: 5,
@@ -60,9 +59,9 @@
             color: '#EEE',
             faces: 12,
             images: [
-                "images/gem-01.gif",
-                "images/gem-03.gif",
-                "images/gem-04.gif",
+                "img/gem-01.gif",
+                "img/gem-03.gif",
+                "img/gem-04.gif"
             ],
             reviews: [{
                 stars: 3,
@@ -85,9 +84,9 @@
             color: '#000',
             faces: 6,
             images: [
-                "images/gem-06.gif",
-                "images/gem-07.gif",
-                "images/gem-09.gif"
+                "img/gem-06.gif",
+                "img/gem-07.gif",
+                "img/gem-09.gif"
             ],
             reviews: [{
                 stars: 1,
